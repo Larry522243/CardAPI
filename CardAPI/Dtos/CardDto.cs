1e18263dd10eb209d6c737906f85ad48013e03bb
Larry522243 <larry268782@gmail.com>
2022-09-28 02:15:28 +0800

    放回正確位置

--- CardAPI/Dtos/CardDto.cs
+++ CardAPI/Dtos/CardDto.cs
@@ -27,35 +27,35 @@ namespace CardAPI.Dtos
         /// </summary>
         public int Health { get; set; }
     }
-}
-/// <summary>
-/// 卡片
-/// </summary>
-public class CardPartialDto
-{
     /// <summary>
-    /// 卡片編號
+    /// 卡片
     /// </summary>
-    public int Id { get; set; }
-    /// <summary>
-    /// 卡片描述
-    /// </summary>
-    public string? Description { get; set; }
-    /// <summary>
-    /// /// 攻擊力
-    /// </summary>
-    public int Attack { get; set; }
-    /// <summary>
-    /// 血量
-    /// </summary>
-    public int Health { get; set; }
-    /// <summary>
-    /// 花費
-    /// </summary>
-    public int? Cost { get; set; } = 0;
-    /// <summary>
-    /// 等級
-    /// </summary>
-    public CardLevel Level { get; set; } = CardLevel.Poor;
+    public class CardPartialDto
+    {
+        /// <summary>
+        /// 卡片編號
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 卡片描述
+        /// </summary>
+        public string? Description { get; set; }
+        /// <summary>
+        /// /// 攻擊力
+        /// </summary>
+        public int Attack { get; set; }
+        /// <summary>
+        /// 血量
+        /// </summary>
+        public int Health { get; set; }
+        /// <summary>
+        /// 花費
+        /// </summary>
+        public int? Cost { get; set; } = 0;
+        /// <summary>
+        /// 等級
+        /// </summary>
+        public CardLevel Level { get; set; } = CardLevel.Poor;
+    }
 }
 
